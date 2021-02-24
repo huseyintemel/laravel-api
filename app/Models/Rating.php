9d3336399aecd76c505d904cd6c5a026886d6e79
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-24 18:12:30 +0300

    first commit

---
+++ app/Models/Rating.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Book;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'rating'
+    ];
+
+
+    public function book()
+    {
+      return $this->belongsTo(Book::class);
+    }
+}
